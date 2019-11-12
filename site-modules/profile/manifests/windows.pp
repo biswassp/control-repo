8072629b4eca06dbc111b8bf8252919cd0f78232
biswassp <41198933+biswassp@users.noreply.github.com>
2019-11-12 14:32:31 +0800

    Update windows.pp

--- site-modules/profile/manifests/windows.pp
+++ site-modules/profile/manifests/windows.pp
@@ -15,4 +15,8 @@ local_security_policy { 'Allow log on locally':
   ensure => 'present',
   policy_value => 'Administrators, Users, puppetuser01, puppetuser02',
 }
+local_security_policy { 'Maximum password age':
+  ensure => present,
+  policy_value => '90',
+}
 }
