1b6e9a94e3aa383e3285fbf472f7692cef5c4516
biswassp <41198933+biswassp@users.noreply.github.com>
2019-11-12 15:12:18 +0800

    Update createdirectory.pp

--- site-modules/profile/manifests/createdirectory.pp
+++ site-modules/profile/manifests/createdirectory.pp
@@ -4,4 +4,15 @@ file { 'c:/puppet':
 #  owner  => 'puppetuser02',
 #  group  => 'puppetuser02',
 }
+acl { 'c:/puppet':
+  target                     => 'c:/puppet',
+  purge                      => false,
+  permissions                => [
+   { identity => 'puppetuser01', rights => ['full'], perm_type=> 'allow', child_types => 'all', affects => 'all' },
+   { identity => 'puppetuser02', rights => ['read'], perm_type=> 'allow', child_types => 'all', affects => 'all' }
+  ],
+  owner                      => 'puppetuser01', #Creator_Owner specific, doesn't manage unless specified
+  group                      => 'puppetuser02', #Creator_Group specific, doesn't manage unless specified
+  inherit_parent_permissions => true,
+}
 }
