13a59ec3e4ed384a1d8199fa0894f105cc2708ed
biswassp <41198933+biswassp@users.noreply.github.com>
2019-11-13 10:49:00 +0800

    Create basicweb.pp

---
+++ site-modules/profile/manifests/basicweb.pp
@@ -0,0 +1,18 @@
+class profile::basicweb{
+# Delete the default website to prevent a port binding conflict.
+iis_site {'Default Web Site':
+  ensure  => absent,
+}
+
+iis_site { 'minimal':
+  ensure          => 'started',
+  physicalpath    => 'c:\\inetpub\\basicweb',
+  applicationpool => 'DefaultAppPool',
+  require         => [File['minimal'],],
+}
+
+file { 'minimal':
+  ensure => 'directory',
+  path   => 'c:\\inetpub\\minimal',
+}
+}
